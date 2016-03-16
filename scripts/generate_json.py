c61dcf28432f5443a13aadf8ba88d2c90fbb264d
Martijn van Exel <m@rtijn.org>
2016-03-16 16:24:36 -0600

    added state web sites (verified)

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import simplejson as json
-
-states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC"]
+import csv
 
 dots = {}
 
-for state in states:
-	dots[state] = {
-		"website": "",
-		"twitter": "",
-		"rss": ""
-	}
+with open('state_table.csv', 'r') as states_in:
+	states = csv.reader(states_in)
+	for state in states:
+		dots[state[0]] = {
+			"website": state[2],
+			"twitter": "",
+			"rss": ""
+		}
 
 with open('dot-feed.json', 'w') as fhandle:
 	json.dump(dots, fhandle, sort_keys=True, indent='    ')
